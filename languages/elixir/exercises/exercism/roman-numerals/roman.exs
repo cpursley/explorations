d39ac6cf23ba7e66577217b8b080c95eb14c6494
Chase Pursley <chasepursley@gmail.com>
2019-04-05 08:27:34 -0400

    [ELIXIR] Exercises#exercism: 'Roman Numerals' - take II.

--- languages/elixir/exercises/exercism/roman-numerals/roman.exs
+++ languages/elixir/exercises/exercism/roman-numerals/roman.exs
@@ -5,26 +5,20 @@ defmodule Roman do
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
     cond do
-      number in 1..3     -> String.duplicate("I", number)
-      number == 4        -> "IV"
-      number in 5..8     -> "V" <> String.duplicate("I", number - 5)
-      number == 9        -> "IX"
-      number in 10..39   -> duplicate_and_remainder(number, 10, "X")
-      number in 40..49   -> "XL" <> remainder(number, 10)
-      number in 50..89   -> "L" <> duplicate_and_remainder(number - 50, 10, "X")
-      number in 90..99   -> "XC" <> remainder(number, 10)
-      number in 100..399 -> duplicate_and_remainder(number, 100, "C")
-      number in 400..499 -> "CD" <> remainder(number, 100)
-      number in 500..889 -> "D" <> duplicate_and_remainder(number - 500, 100, "D")
-      number in 890..999 -> "CM" <> remainder(number, 100)
-      number >= 1000     -> duplicate_and_remainder(number, 1000, "M")
-      true               -> ""
+      number >= 1000 -> "M"  <> numerals(number - 1000)
+      number >= 900  -> "CM" <> numerals(number - 900)
+      number >= 500  -> "D"  <> numerals(number - 500)
+      number >= 400  -> "CD" <> numerals(number - 400)
+      number >= 100  -> "C"  <> numerals(number - 100)
+      number >= 90   -> "XC" <> numerals(number - 90)
+      number >= 50   -> "L"  <> numerals(number - 50)
+      number >= 40   -> "XL" <> numerals(number - 40)
+      number >= 10   -> "X"  <> numerals(number - 10)
+      number >= 9    -> "IX" <> numerals(number - 9)
+      number >= 5    -> "V"  <> numerals(number - 5)
+      number >= 4    -> "IV" <> numerals(number - 4)
+      number >= 1    -> "I"  <> numerals(number - 1)
+      true           ->  ""
     end
-  end
-
-  def remainder(number, divisor), do: number |> rem(divisor) |> numerals
-
-  def duplicate_and_remainder(number, divisor, numeral) do
-    String.duplicate(numeral, trunc(number / divisor)) <> remainder(number, divisor)
   end
 end
